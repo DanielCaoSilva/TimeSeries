0b9412e4b898a5a93a878642ff379f2e4ed7e8a3
Daniel Cao Silva <47014112+DanielCaoSilva@users.noreply.github.com>
2021-11-16 00:56:29 -0800

    Add files via upload
    Reads the historical data now with missing data interpolated. Still need to determine optimal seasonality and differencing.

--- readingBuoyDataV2.R
+++ readingBuoyDataV2.R
@@ -5,77 +5,80 @@ library(lubridate)
 
 #wav_data <-read.csv("https://www.ndbc.noaa.gov/data/realtime2/46268.txt")
 #b_data <- read.table("https://www.ndbc.noaa.gov/data/realtime2/46268.txt")
+#b_data <- read.table("https://www.ndbc.noaa.gov/data/realtime2/46268.txt")
 b_data <- read.table("https://www.ndbc.noaa.gov/view_text_file.php?filename=46221h2020.txt.gz&dir=data/historical/stdmet/")
 #V1   2  3  4  5   6    7   8     9     10    11  12     13   14    15    16    17  18    19
 #YY  MM DD hh mm WDIR WSPD GST  WVHT   DPD   APD MWD   PRES  ATMP  WTMP  DEWP  VIS PTDY  TIDE
 #yr  mo dy hr mn degT m/s  m/s     m   sec   sec degT   hPa  degC  degC  degC  nmi  hPa    ft
-# b_data[,5] = b_data[,5]+4 
 
+
+#Format the dates using a Date Class
 b_date <- paste(b_data[,1],"-",b_data[,2],"-",b_data[,3]," ",b_data[,4],":",b_data[,5]) 
-#b_date = paste(b_data[,1],"-",b_data[,2],"-",b_data[,3]," ",b_data[,4],":",00,sep="") ":",00,sep="") 
-
 b_date <- ymd_hm(b_date,tz = "UTC")
-b_date <- b_date + minutes(4)
+
+#7693 to 7694 is when the change of intervals happens 17:26 to 19:53
+#15416 to 15417 is when the change of intervals happens 20:23 to 22:26
+b_date <- b_date
 head(b_date)
 
 b_data %>% select(V9) -> data
 data$time <- b_date
 
-# date1 <- ymd_hms("2020-01-01 00:26:00")
-# date2 <- ymd_hms("2020-01-01 00:56:00")
-
-# b_tbl <- tibble(
-#   time = interval(date1,date2),
-#   name = 'Significant Wave Height',
-#   value = data
-# )
-x <- seq(as.interval(b_date,ymd_hms("2020-01-01 00:30:00")))
-buoy_ts <- as_tsibble(data,index=time,key = starts_with("2020-01-01 00:30:00"),regular = TRUE)
-
-# all_of(data) %>% 
-#   mutate(time = ceiling_date(time, unit = "30 mins")) %>%
-#   as_tsibble(data,regular=TRUE) -> buoy_ts
+#Attempt to fix the uneven time intervals as select times
+testies<-data$time[7694:15416]
+fixedTimes <- testies + minutes(3)
+data$time[7694:15416] <- fixedTimes
 
 
+which((data$time[2:16184]-data$time[1:16183])>30)
 
+# Turn into a tsibble 
+# using build_tsibble instead of as_tsibble to specifiy the interval  
+buoy_ts <- build_tsibble(
+  data,
+  key = dplyr::starts_with("2020-01-01 00:26:00"),
+  index = time,
+  ordered = TRUE,
+  interval = new_interval(minute = 30),
+  validate = TRUE
+)
+
+# Count all the missing data and make them NA values
 buoy_gaps <- buoy_ts %>%
   count_gaps(.full = TRUE)
-# buoy_ts <- buoy_ts %>% fill_gaps(.full = TRUE)
+buoy_ts <- buoy_ts %>% fill_gaps()
 
+#Try fixing missing data using interpolate
+buoy_fill <- buoy_ts %>% 
+  model(ARIMA(V9)) %>%
+  interpolate(buoy_ts)
+
+#Test Plot the data 
 buoy_ts %>% autoplot()
 buoy_ts %>% gg_tsdisplay(V9,plot_type="partial")
 
-# buoy_ts %>% mutate(log = log(V9)) %>%
-#   mutate(diff = difference(V9)) %>%
-#   mutate(diff2 = difference(diff)) %>%
-#   mutate(logDif = difference(log)) %>%
-#   mutate(logDif2 = difference(logDif)) -> buoy_ts
-# buoy_ts %>% gg_tsdisplay(V9,"partial")
-# buoy_ts %>% gg_tsdisplay(log,"partial")
-# buoy_ts %>% gg_tsdisplay(diff,"partial")
-# buoy_ts %>% gg_tsdisplay(diff2,"partial")
-# buoy_ts %>% gg_tsdisplay(logDif,"partial")
-# buoy_ts %>% gg_tsdisplay(logDif2,"partial")
+#STL decomp 
+buoy_fill %>%
+  model(
+    STL(V9 ~ trend(window = 7)+
+          season(window = "periodic"),
+        robust = TRUE)) %>%
+  components()%>%
+  autoplot()
+  
 
-# fit2 <-buoy_ts %>%
-#   model(m1 = ARIMA(log ~ pdq(1,0,1)),
-#         m2 = ARIMA(log ~ pdq(2,0,0)),
-#         m3 = ARIMA(log ~ pdq(2,0,1)),
-#         m4 = ARIMA(log ~ pdq(3,0,2)),)
-# glance(fit2) %>% arrange(AICc)
-
-
-# fit <- buoy_ts %>% 
-#   model(arima = ARIMA(log ~ 0 + pdq(0,1,1) +PDQ(0,1,1))) %>% 
-#   report()
-
-#wav_data_time %>% transform(test2,time=paste0(V1,V2,V3)) 
-# buoy_ts %>% tsibble::update_tsibble(.data, regular = TRUE)
-
-fit <- buoy_ts %>% model(auto = ARIMA(V9,stepwise=FALSE,approximation=FALSE)) 
+# Model Fitting 
+fit <- buoy_fill %>% model(auto = ARIMA(V9))
+#                          m1 = ARIMA(log(V9) ~ 0 + pdq(1,1,0) +PDQ(2,0,2))+season("2") )
 report(fit)
 glance(fit)
- 
+
+
+# Forecast 
+fit %>% forecast(h=1000) %>% autoplot(buoy_fill)
+report(fit)
+
+
 
 
 
